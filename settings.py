fc64021e90152db0200ca0b8811ee0652adbae5b
KaranKulx ✌️ <rajprog007@gmail.com>
2024-02-04 17:47:52 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,3 @@
 # MongoDB attributes
-mongodb_uri = 'mongodb+srv://karankulx:KAran798322@gyaan.wqn4gkz.mongodb.net/?retryWrites=true&w=majority'
+mongodb_uri = 'mongodb+srv://karankulx:password@gyaan.wqn4gkz.mongodb.net/?retryWrites=true&w=majority'
 port = 8000
